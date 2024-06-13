954fb42115333521675ca0187bf9627fb3d703fa
Jacob Johnson <jacob.w.johnson12@icloud.com>
2024-06-12 23:06:46 -0600

    Finished class work for Assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,15 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = ["😅", "🙃", "🤦‍♂️", "😆"]
+    let emojis: [String] = ["😅", "🙃", "🤦‍♂️", "😆", "☺️", "🙄", "😍", "😇", "😉", "🤞", "🥹", "🤣"]
+    
+    @State var cardCount: Int = 4
+    
     var body: some View {
-        HStack {
-            ForEach(emojis.indices,id: \.self) {
+        VStack{
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus")
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) {
                 index in CardView(cardEmoji: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
     }
 }
 
@@ -26,15 +67,16 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let cardbase = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-            if isFaceUp {
+            Group {
                 cardbase
                     .fill(.white)
                     .strokeBorder(style: StrokeStyle(lineWidth: 5))
                 Text(cardEmoji).font(.largeTitle)
-            } else {
-                cardbase.fill(.blue)
             }
-        }.onTapGesture {
+            .opacity(isFaceUp ? 1 : 0)
+            cardbase.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
             isFaceUp.toggle()
         }
     }
